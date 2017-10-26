a42ff4efe40c86bf998d442c992f7c7170194e3c
Edi Carlos note <edi_csilva@hotmail.com>
2017-10-26 10:00:01 -0200

    fim conhecendo os componentes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
-// $(function() {
-//     // equivalente ao load da página
-//     alert("olá!");
-// });
+$(function() {
+    // equivalente ao load da página
+    // alert("olá!");
+
+    $("#modal01").modal({ backdrop: false, keyboard: false });
+
+    $("#modal01").modal("show");
+
+    setTimeout(function() {
+        $("#modal01").modal("hide");
+    }, 3000);
+
+    $(".ttp").tooltip({
+        title: "Título padrão",
+        trigger: "click"
+    });
+
+    $(".ppv").popover({
+        title: "Título do popover",
+        trigger: "hover focus"
+    });
+
+    $(".btnjs").button();
+
+    $("#troca-estado").click(function() {
+        var btn = $(this);
+        btn.button("loading");
+
+        setTimeout(function() {
+            btn.button("reset");
+        }, 3000);
+    });
+
+});
